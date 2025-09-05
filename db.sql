f4005fd045129a9ccdfa155a77b07389b3a312e8
Baumann001 <gustavo_baumann@estudante.sesisenai.org.br>
2025-09-05 09:01:28 -0300

    add- algumas correções

--- db.sql
+++ db.sql
@@ -1,43 +1,45 @@
-CREATE DATABASE petshop_db;
+
+CREATE DATABASE  petshop_db;
 USE petshop_db;
 
 
-CREATE TABLE  cliente(
-id INT AUTO_INCREMENT PRIMARY KEY,
-nome VARCHAR(100)NOT NULL,
-cpf VARCHAR(14)NOT NULL  UNIQUE,
-telefone VARCHAR(15) NOT NULL,
-email VARCHAR(100)
+CREATE TABLE cliente (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    cpf VARCHAR(14) NOT NULL UNIQUE,
+    telefone VARCHAR(15),
+    email VARCHAR(100)
 );
 
-CREATE TABLE pet(
-id AUTO_INCREMENT PRIMARY KEY,
-cliente_id INT NOT NULL,
-nome VARCHAR(100) NOT NULL,
-especie ENUM('cachorro',  'gato','ave','roedor','outro') NOT NULL,
-porte ENUM('pequeno','medio','grande') NOT NULL,
-nascimento DATE,
-FOREIGN KEY (cliente_id) REFERENCES cliente(id)
 
+CREATE TABLE pet (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    cliente_id INT NOT NULL,
+    nome VARCHAR(100) NOT NULL,
+    especie ENUM('cachorro', 'gato', 'ave', 'roedor', 'outro') NOT NULL,
+    porte ENUM('pequeno', 'medio', 'grande') NOT NULL,
+    nascimento DATE,
+    FOREIGN KEY (cliente_id) REFERENCES cliente(id)
 );
+
 
 CREATE TABLE servico (
-id INT AUTO_INCREMENT  PRIMARY KEY,
-nome VARCHAR(100) NOT NUlL,
-preco DECIMAL(8,2) NOT NULL,
-duracao_min INT NOT NULL,
-CHECK(preco >=0),
-CHECK(duracao_min>0)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL UNIQUE,
+    preco DECIMAL(8, 2) NOT NULL,
+    duracao_min INT NOT NULL,
+    CHECK (preco >= 0),
+    CHECK (duracao_min > 0)
 );
 
-CREATE TABLE agendamento(
-id INT AUTO_INCREMENT PRIMARY KEY,
-pet_id INT NOT NULL,
-servico_id INT NOT NULL,
-data_hora DATETIME NOT NULL,
-status ENUM('agendado', 'concluido', 'cancelado') NOT NULL,
+
+CREATE TABLE agendamento (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    pet_id INT NOT NULL,
+    servico_id INT NOT NULL,
+    data_hora DATETIME NOT NULL,
+    status ENUM('agendado', 'concluido', 'cancelado') NOT NULL,
     observacoes VARCHAR(255),
     FOREIGN KEY (pet_id) REFERENCES pet(id),
     FOREIGN KEY (servico_id) REFERENCES servico(id)
-
 );
