205db7b94f427208eca4c719a2bd9839ebedac1f
Baumann001 <gustavo_baumann@estudante.sesisenai.org.br>
2025-09-05 12:51:55 -0300

    add-

--- create_cliente.php
+++ create_cliente.php
@@ -1,6 +1,56 @@
 <?php
 
+include 'db.php';
 
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    
+
+$nome = $_POST['nome'];
+$id = $_POST['id'];
+$cpf = $_POST['cpf'];
+$telefone  = $_POST['telefone'];
+$email = $_POST['email'];
+
+$sql = "INSERT INTO cliente (nome, cpf, telefone, email) VALUES ('$nome', '$cpf', '$telefone', '$email')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Cliente inserido com sucesso!";
+} else {
+    echo "Erro: " . $sql . "<br>" . $conn->error;
+}
+
+};
 
 
 ?>
+
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<form action="create.php" method="post">
+
+<label for="nome">Nome:</label>
+<input type="text" name = "nome" required>
+
+<label for="cpf">CPF:</label>
+<input type="text" name = "cpf" required>
+
+<label for="telefone">Telefone:</label>
+<input type="text" name = "telefone" required>
+
+<label for="email">Email:</label>
+<input type="email" name = "email" required>
+
+
+<input type="submit" value = "Adicionar">
+
+</form>
+    
+</body>
+</html>
